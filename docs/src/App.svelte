ecc11f02a3dde1565d42a0cec7a37ea422e2a5ce
Nate d <ndelrosx6@gmail.com>
2024-03-10 16:05:42 -0700

    Add files via upload

--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -101,6 +101,18 @@
         }
       });
 
+      mapTokyo.on('mousemove', 'tokyo-population-density', (e) => {
+        if (e.features.length > 0) {
+          const feature = e.features[0];
+          const population = feature.properties.population; // Assuming there's a 'population' property in your geojson data
+          const wardName = feature.properties.N03_004;
+          const popupText = population ? `${wardName} Population: ${population}` : 'No population data available';
+          popup.setLngLat(e.lngLat)
+              .setText(popupText)
+              .addTo(mapTokyo);
+        }
+      });
+
       const response = await fetch('/stations.json'); 
       const stationsData = await response.json(); 
 
@@ -160,6 +172,23 @@
     border-radius: 5px;
     box-shadow: 0 2px 4px rgba(0,0,0,0.2);
   }
+  .legend {
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+  padding: 5px;
+}
+
+.legend-item {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  padding: 2px 5px;
+  font-size: 12px;
+}
 </style>
 
 <div id="map-tokyo">
@@ -176,3 +205,12 @@
 <button class="toggle-button" on:click={toggleChoroplethVisibility} style="top: 140px;">
   {isChoroplethVisible ? 'Hide Choropleth' : 'Show Choropleth'}
 </button>
+
+<div class="legend" style="bottom: 200px;">
+  <div class="legend-item" style="background-color: #ffffff;">0</div>
+  <div class="legend-item" style="background-color: #614144;">100,000</div>
+  <div class="legend-item" style="background-color: #802A37;">300,000</div>
+  <div class="legend-item" style="background-color: #A51C2A;">500,000</div>
+  <div class="legend-item" style="background-color: #DE0D0D;">700,000</div>
+  <div class="legend-item" style="background-color: #FF9641;">1,000,000</div>
+</div>
